3a595964196e0d1025abd055016d6d4986373f8f
Lindsey Sugino <lindseysugino@gmail.com>
2014-02-10 17:44:09 -0800

    updated output layout

--- BeerSong.java
+++ BeerSong.java
@@ -5,10 +5,8 @@ public class BeerSong {
 
 		while (beerNum > 0) {
 
-			System.out.println(beerNum + " " + word + " beer on the wall");
-			System.out.println(beerNum + " " + word + " of beer.");
-			System.out.println("Take one down.");
-			System.out.println("Pass it around.");
+			System.out.println(beerNum + " " + word + " beer on the wall, " + beerNum + " " + word + " of beer.");
+			System.out.print("Take one down, pass it around, ");
 			beerNum = beerNum - 1;
 
 			if (beerNum == 1) {
@@ -16,9 +14,9 @@ public class BeerSong {
 			}
 
 			if (beerNum > 0) {
-				System.out.println(beerNum + " " + word + " of beer on the wall");
+				System.out.println(beerNum + " " + word + " of beer on the wall.");
 			} else {
-				System.out.println("No more bottles of beer on the wall! :(");
+				System.out.println("no more bottles of beer on the wall! :(");
 			}
 
 		}
